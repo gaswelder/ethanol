49ff9622271e8d1162d3b6ecd45d3becbf5812e7
gas <gasweld@gmail.com>
2018-08-04 22:58:08 +0300

    Cleaner test

--- tests/test.js
+++ tests/test.js
@@ -14,31 +14,35 @@ tap.test("main", async function(t) {
 
 	const coin = await deploy(god, "tests/TokenERC20", [100, "Testcoin", "TST"]);
 
-	t.equal(
-		(await god.read(coin, "balanceOf", [god.address()])).toString(),
-		"100000000000000000000"
-	);
-	t.equal((await god.read(coin, "name")).toString(), "Testcoin");
-	t.equal(
-		(await alice.read(coin, "balanceOf", [alice.address()])).toString(),
-		"0"
-	);
-	t.equal(
-		(await god.read(coin, "balanceOf", [alice.address()])).toString(),
-		"0"
-	);
+	t.test("balances", async function(t) {
+		t.equal(
+			(await god.read(coin, "balanceOf", [god.address()])).toString(),
+			"100000000000000000000"
+		);
+		t.equal((await god.read(coin, "name")).toString(), "Testcoin");
+		t.equal(
+			(await alice.read(coin, "balanceOf", [alice.address()])).toString(),
+			"0"
+		);
+		t.equal(
+			(await god.read(coin, "balanceOf", [alice.address()])).toString(),
+			"0"
+		);
+	});
 
-	const r = await god.call(coin, "transfer", [alice.address(), 1]);
-	const logs = await r.logs();
-	t.ok(logs.length > 0);
-	t.equals(logs[0].event, "Transfer");
+	t.test("direct transfer", async function(t) {
+		const r = await god.call(coin, "transfer", [alice.address(), 1]);
+		const logs = await r.logs();
+		t.ok(logs.length > 0);
+		t.equals(logs[0].event, "Transfer");
 
-	t.equal(
-		(await god.read(coin, "balanceOf", [god.address()])).toString(),
-		"99999999999999999999"
-	);
-	t.equal(
-		(await god.read(coin, "balanceOf", [alice.address()])).toString(),
-		"1"
-	);
+		t.equal(
+			(await god.read(coin, "balanceOf", [god.address()])).toString(),
+			"99999999999999999999"
+		);
+		t.equal(
+			(await god.read(coin, "balanceOf", [alice.address()])).toString(),
+			"1"
+		);
+	});
 });
