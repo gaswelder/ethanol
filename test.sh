b8a1a074e08a3badc382278d603dc8741293d5e3
gasweld <gasweld@gmail.com>
2019-12-21 00:01:40 +0300

    Wait for geth to start before tests

--- test.sh
+++ test.sh
@@ -3,6 +3,11 @@
 geth --datadir ./data --dev --dev.period 1 --rpc --verbosity 2 &
 pid=$!
 
+while [ `ls data | grep geth.ipc | wc -l` -eq '0' ]; do
+    echo 'waiting for geth to start'
+    sleep 1
+done
+
 yarn run tap tests
 
 kill $pid
